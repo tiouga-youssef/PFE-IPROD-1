686a25560820afd5599a0b11f14ca5de4abdebba
tiouga-youssef <60611157+tiouga-youssef@users.noreply.github.com>
2021-07-13 15:26:20 +0100

    Add files via upload

---
+++ src/main/java/com/dxc/iprod/IprodApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.iprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxc/iprod/IprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.iprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
